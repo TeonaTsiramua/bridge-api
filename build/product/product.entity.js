a57116ac6483c200674d4610f08d8838c3b66285
DSenpai <gogniashvili.amz@gmail.com>
2024-05-23 17:34:31 +0400

    removed build and changed version

--- build/product/product.entity.js
+++
@@ -1,5 +0,0 @@
-import { Schema, model } from 'mongoose';
-export const productSchema = new Schema({
-    title: { type: 'String', required: true },
-});
-export const Product = model('Product', productSchema);
