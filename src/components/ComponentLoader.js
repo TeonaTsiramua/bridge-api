84fd9b79d35aa02726b06907beef711e1092c073
DSenpai <gogniashvili.amz@gmail.com>
2024-08-21 00:11:05 +0400

    last try

--- src/components/ComponentLoader.js
+++ src/components/ComponentLoader.js
@@ -1,14 +1,11 @@
 import { ComponentLoader } from 'adminjs';
 import path from "path";
-import { fileURLToPath } from "url";
 
 const componentLoader = new ComponentLoader();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const Components = {
-    ImageUploader: componentLoader.add('ImageUploader', path.resolve(__dirname, "ImageUploader")),
-    ImageShow: componentLoader.add('ImageShow', path.resolve(__dirname, "ImageShow")),
+    ImageUploader: componentLoader.add('ImageUploader', path.resolve('./src/components/ImageUploader.jsx')),
+    ImageShow: componentLoader.add('ImageShow', path.resolve('./src/components/ImageShow.jsx')),
 };
 
 export { componentLoader, Components };
