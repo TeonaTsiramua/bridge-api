10f54cbf96df51c07000fc8b50704cd3eadf8e08
DSenpai <gogniashvili.amz@gmail.com>
2024-08-21 00:04:44 +0400

    tried resolving with absolute paths

--- src/components/ComponentLoader.js
+++ src/components/ComponentLoader.js
@@ -1,10 +1,14 @@
 import { ComponentLoader } from 'adminjs';
+import path from "path";
+import { fileURLToPath } from "url";
 
 const componentLoader = new ComponentLoader();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const Components = {
-    ImageUploader: componentLoader.add('ImageUploader', './ImageUploader.jsx'),
-    ImageShow: componentLoader.add('ImageShow', './ImageShow.jsx'),
+    ImageUploader: componentLoader.add('ImageUploader', path.resolve(__dirname, "ImageUploader")),
+    ImageShow: componentLoader.add('ImageShow', path.resolve(__dirname, "ImageShow")),
 };
 
 export { componentLoader, Components };
